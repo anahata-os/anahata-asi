1524a4e3e7621c6317c193c24ec387ee1229309f
priyanahata <p@anahata.uno>
2025-11-23 11:32:15 +0100

    prunning

--- anahata-ai/src/main/java/uno/anahata/ai/model/tool/AbstractToolCall.java
+++ anahata-ai/src/main/java/uno/anahata/ai/model/tool/AbstractToolCall.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Anahata Software License (ASL) V2.0;
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may not obtain a copy of the License at
  *
  *      https://github.com/pablo-anahata/anahata-ai-parent/blob/main/LICENSE
  *
@@ -82,11 +82,35 @@ public abstract class AbstractToolCall<T extends AbstractTool, R extends Abstrac
      */
     protected abstract R createResponse();
     
+    //<editor-fold defaultstate="collapsed" desc="V3 Context Management Delegation">
     @Override
     protected int getDefaultTurnsToKeep() {
         // A tool call's lifecycle is always identical to its response.
         return getResponse().getDefaultTurnsToKeep();
     }
+    
+    @Override
+    public boolean isEffectivelyPruned() {
+        return getResponse().isEffectivelyPruned();
+    }
+    
+    @Override
+    public int getTurnsLeft() {
+        return getResponse().getTurnsLeft();
+    }
+    
+    @Override
+    public void setPruned(Boolean pruned) {
+        super.setPruned(pruned);
+        getResponse().setPruned(pruned);
+    }
+    
+    @Override
+    public void setTurnsToKeep(Integer turnsToKeep) {
+        super.setTurnsToKeep(turnsToKeep);
+        getResponse().setTurnsToKeep(turnsToKeep);
+    }
+    //</editor-fold>
     
     @Override
     public String asText() {
