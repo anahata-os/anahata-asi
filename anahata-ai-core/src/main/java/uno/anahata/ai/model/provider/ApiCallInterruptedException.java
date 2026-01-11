476e303a74d1238f2fcf27652ac515733777412a
priyanahata <p@anahata.uno>
2026-01-12 00:57:07 +0100

    El Cant del Barça. - anahata-ai-core/src/main/java/uno/anahata/ai/model/provider/ApiCallInterruptedException.java

---
+++ anahata-ai-core/src/main/java/uno/anahata/ai/model/provider/ApiCallInterruptedException.java
@@ -0,0 +1,23 @@
+/* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
+package uno.anahata.ai.model.provider;
+
+/**
+ * Exception thrown when an AI model generation call is interrupted, either
+ * by the user or by the system.
+ * 
+ * @author anahata
+ */
+public class ApiCallInterruptedException extends RuntimeException {
+
+    public ApiCallInterruptedException(String message) {
+        super(message);
+    }
+
+    public ApiCallInterruptedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApiCallInterruptedException(Throwable cause) {
+        super(cause);
+    }
+}
