08fa57a8e6c798163a880c80676c126e5fda7991
priyanahata <p@anahata.uno>
2026-01-22 13:30:42 +0100

    raw args

--- anahata-asi-core/src/main/java/uno/anahata/asi/model/tool/bad/BadToolCall.java
+++ anahata-asi-core/src/main/java/uno/anahata/asi/model/tool/bad/BadToolCall.java
@@ -16,7 +16,7 @@ import uno.anahata.asi.model.tool.AbstractToolCall;
 public class BadToolCall extends AbstractToolCall<BadTool, BadToolResponse> {
 
     public BadToolCall(AbstractModelMessage amm, @NonNull String id, @NonNull BadTool tool, @NonNull Map<String, Object> args) {
-        super(amm, id, tool, args);
+        super(amm, id, tool, args, args); // For bad tools, raw and enriched args are the same.
     }
 
     @Override
