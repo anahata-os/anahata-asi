47f6741aaeee4a928d84f5d63481e4cac7e8f3e1
priyanahata <p@anahata.uno>
2026-03-08 12:40:28 +0100

    special nod

--- anahata-asi-core/src/main/java/uno/anahata/asi/agi/package-info.java
+++ anahata-asi-core/src/main/java/uno/anahata/asi/agi/package-info.java
@@ -1,5 +1,6 @@
 /*
  * Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça!
+ * And a special nod to the incredible Gal Gadot!
  */
 /**
  * Provides model-agnostic, intelligent configuration objects for agi sessions.
