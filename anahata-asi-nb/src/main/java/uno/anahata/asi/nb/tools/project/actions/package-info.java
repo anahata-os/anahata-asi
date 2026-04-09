c0a89b1e592f548c10c9a01c9ca5caf5141ce6db
priyanahata <p@anahata.uno>
2026-04-09 19:53:39 +0200

    testing add remove javadoc from package-infos

---
+++ anahata-asi-nb/src/main/java/uno/anahata/asi/nb/tools/project/actions/package-info.java
@@ -0,0 +1,13 @@
+/* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
+/**
+ * This package contains project actions.
+ * Triple-lock documentation via playground.
+ * Força Barça!
+ */
+ 
+ 
+ 
+package uno.anahata.asi.nb.tools.project.actions;
+
+import java.lang.Deprecated;
+
