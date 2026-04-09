018de6b31bc9fd5151cb314acbf74c468b836708
priyanahata <p@anahata.uno>
2026-04-09 23:05:48 +0200

    javadoc

--- anahata-asi-nb/src/main/java/uno/anahata/asi/nb/tools/project/actions/package-info.java
+++ anahata-asi-nb/src/main/java/uno/anahata/asi/nb/tools/project/actions/package-info.java
@@ -1,13 +1,11 @@
 /* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
+
 /**
- * This package contains project actions.
- * Triple-lock documentation via playground.
- * Força Barça!
+ * NetBeans IDE actions for managing project context within AGI sessions.
+ * <p>
+ * This package provides the UI actions and underlying logic to dynamically add or 
+ * remove NetBeans projects from the context of active AGI sessions. These actions
+ * are integrated into the NetBeans project explorer's context menus.
+ * </p>
  */
- 
- 
- 
 package uno.anahata.asi.nb.tools.project.actions;
-
-import java.lang.Deprecated;
-
