aaa7bbd3e6199d40f7eecda5590bad2a636c8afd
priyanahata <p@anahata.uno>
2026-05-11 16:56:58 +0200

    web crawling drone swarms

--- anahata-asi-yam/src/main/java/uno/anahata/asi/yam/tools/chrome/ChromeUtils.java
+++ anahata-asi-yam/src/main/java/uno/anahata/asi/yam/tools/chrome/ChromeUtils.java
@@ -1,5 +1,5 @@
 /* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
-package uno.anahata.asi.yam.tools;
+package uno.anahata.asi.yam.tools.chrome;
 
 import java.io.File;
 import org.apache.commons.lang3.SystemUtils;
