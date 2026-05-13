b806446ddecd1b2883075ccb316656fdda48abc5
priyanahata <p@anahata.uno>
2026-05-13 20:30:53 +0200

    @AgiToolParam annotation

--- anahata-asi-core/src/main/java/uno/anahata/asi/toolkit/resources/Resources.java
+++ anahata-asi-core/src/main/java/uno/anahata/asi/toolkit/resources/Resources.java
@@ -103,7 +103,7 @@ public class Resources extends AnahataToolkit {
     @AgiTool(value = "Loads multiple resources into the context by their URIs.", permission = ToolPermission.APPROVE_ALWAYS)
     public List<String> loadResources(
             @AgiToolParam("The full URIs of the resources.") List<String> uriStrings,
-            @AgiToolParam(value = "Initial viewport settings for text resources. If not provided, it uses the system default viewport (65K chars 1024 col width)", required = false) TextViewportSettings initialSettings) throws Exception {
+            @AgiToolParam(value = "Initial viewport settings for text resources. If not provided, it uses the system default viewport (0-65K chars, 1024 chars col width incluedLines=true)", required = false) TextViewportSettings initialSettings) throws Exception {
 
         List<Resource> toRegister = new ArrayList<>();
         List<String> ids = new ArrayList<>();
