142d1f0322a55cd1406d3cdedfa7862104aac889
priyanahata <p@anahata.uno>
2026-05-18 17:20:52 +0200

    javadoc

--- anahata-asi-minimax/src/main/java/uno/anahata/asi/minimax/MinimaxAnthropicProvider.java
+++ anahata-asi-minimax/src/main/java/uno/anahata/asi/minimax/MinimaxAnthropicProvider.java
@@ -1,5 +1,6 @@
 package uno.anahata.asi.minimax;
 
+import uno.anahata.asi.AbstractAsiContainer;
 import uno.anahata.asi.anthropic.AnthropicProvider;
 
 /**
@@ -22,7 +23,8 @@ public class MinimaxAnthropicProvider extends AnthropicProvider {
      * </p>
      */
     public MinimaxAnthropicProvider() {
-        super("Minimax", "MiniMax", "https://api.minimax.io/anthropic/v1", "2023-06-01", "https://platform.minimax.io/user-center/basic-information/interface-key");
+        super("Minimax", "MiniMax (Antropic)", "https://api.minimax.io/anthropic/v1", "2023-06-01", "https://platform.minimax.io/user-center/basic-information/interface-key");
+        setFolderName(AbstractAsiContainer.getWorkDirSubDir("Minimax").toString());
     }
 
     /**
@@ -34,6 +36,9 @@ public class MinimaxAnthropicProvider extends AnthropicProvider {
     @Override
     public String getApiKeyHint() {
         return "# MiniMax API Key Configuration\n"
-                + "ey...\n";
+                + "sk-api-ddYQ_z36...//main\n"
+                + "sk-api-I3TuXlWN...//backup\n"
+                + "sk-api-I3Tsdsd1...//backup of the backup\n"
+                ;
     }
 }
