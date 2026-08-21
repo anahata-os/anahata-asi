85d06dc313160d2e6c209d2790751a8dfd3212e2
Priyanka <p@anahata.uno>
2026-08-21 12:21:30 +0200

    missing package info

---
+++ anahata-asi-yam/src/main/java/uno/anahata/asi/yam/games/asikart/package-info.java
@@ -0,0 +1,8 @@
+/* Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça! */
+
+/**
+ * Experimental AI generated Mario Kart like game using java native gaming libraries such as LWJGL.
+ * 
+ * @author anahata
+ */
+package uno.anahata.asi.yam.games.asikart;
