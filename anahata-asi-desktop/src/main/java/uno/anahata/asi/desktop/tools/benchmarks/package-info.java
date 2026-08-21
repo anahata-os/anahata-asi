aa4743832f31831ab44712f5e968cfaed42ec61c
Priyanka <p@anahata.uno>
2026-08-21 18:17:39 +0200

    deleting benchmark stuff

--- anahata-asi-desktop/src/main/java/uno/anahata/asi/desktop/tools/benchmarks/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Licensed under the Anahata Software License (ASL) v 108. See the LICENSE file for details. Força Barça!
- */
-
-/**
- * Programmatic benchmark orchestration tools, test definitions, and catalogs for Anahata-AGI-1.
- */
-package uno.anahata.asi.desktop.tools.benchmarks;
